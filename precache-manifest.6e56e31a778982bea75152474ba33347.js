0bb45d266db099e4147a79955b523b224a480df1
Rahul Sambari <rsambari@paywith.com>
2020-09-01 00:14:40 -0700

    Updates

---
+++ precache-manifest.6e56e31a778982bea75152474ba33347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6fad53f3cae06291d3fb7cd8ccc3327",
+    "url": "/teamsplit/index.html"
+  },
+  {
+    "revision": "f024266c604e7c0156dc",
+    "url": "/teamsplit/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a72a75ac9217a16d4d2f",
+    "url": "/teamsplit/static/js/2.273fee02.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/teamsplit/static/js/2.273fee02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f024266c604e7c0156dc",
+    "url": "/teamsplit/static/js/main.2bcfa747.chunk.js"
+  },
+  {
+    "revision": "400dc801b0a663c3d044",
+    "url": "/teamsplit/static/js/runtime-main.4e3adf74.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b47889256dd48479d5b599de08c4243a.js"
+  "/teamsplit/precache-manifest.6e56e31a778982bea75152474ba33347.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teamsplit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
